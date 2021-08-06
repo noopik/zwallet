9e661d10b0656114f4f725485aa4bd46b858737f
unknown <bakamegane74@gmail.com>
2021-08-06 18:10:25 +0700

    coba commit git make branch baru

--- src/config/Router/index.js
+++ src/config/Router/index.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Homepage, UserLogin } from '../../page';
+import { Homepage, UserLogin, Pagecoba } from '../../page';
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/login" component={UserLogin} />
+        <Route path="/coba" component={Pagecoba} />
       </Switch>
     </Router>
   );
--- src/page/index.js
+++ src/page/index.js
@@ -1,5 +1,7 @@
 import About from './About';
 import Homepage from './Homepage';
+import Pagecoba from './Pagecoba';
 import UserLogin from './UserLogin';
 
-export { Homepage, About, UserLogin };
+
+export { Homepage, About, UserLogin, Pagecoba };
