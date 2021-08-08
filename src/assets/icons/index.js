d68953988c5684fba6dadead46b8f509b62c3cfa
dwinovic <nd.cahya19@gmail.com>
2021-08-08 17:41:39 +0700

    adding page tranfer to user - web version

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,4 @@
-
+// START = NOVI
 import ICBell from './bell.svg';
 import ICArrowUp from './arrow-up.svg';
 import ICBox from './grid.svg';
@@ -7,11 +7,15 @@ import ICUser from './user.svg';
 import ICLogout from './log-out.svg';
 import ICDownGreen from './arrow-down-green.png';
 import ICUpRed from './arrow-up-red.png';
+import ICPen from './search.svg';
+// START = NOVI
+
 // START = NISA
 import SplashLine from './splash-line.png';
 import Success from './success.png';
 // END = NISA
 export {
+  ICPen,
   ICBell,
   ICArrowUp,
   ICBox,
@@ -23,4 +27,3 @@ export {
   SplashLine,
   Success,
 };
-
--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,15 +1,13 @@
-
-
-
-
 import CardProfileUser from './CardProfileUser';
 import LogoBrand from './LogoBrand';
 import Cardwrapper from './CardWrapper';
 import Button from './Button';
 import FieldToken from './FieldToken';
 import FieldWithIcon from './FieldWithIcon';
+import Input from './Input';
 
 export {
+  Input,
   LogoBrand,
   CardProfileUser,
   Cardwrapper,
