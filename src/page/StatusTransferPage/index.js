506e1f00885924c73876982f59e443fe71f2fb4f
Farrel Varian Eka Putra <42968960+farrelvarian@users.noreply.github.com>
2021-08-12 01:05:32 +0700

    set pdf

--- src/page/StatusTransferPage/index.js
+++ src/page/StatusTransferPage/index.js
@@ -10,12 +10,119 @@ import {
 } from '../../assets';
 import { Button, Cardwrapper, SmallCard } from '../../components';
 import { StyleStatus } from './StyleStatus';
+import { jsPDF } from "jspdf";
 
 const StatusTransferPage = () => {
   const history = useHistory();
   const transferState = useSelector((state) => state.transferReducer);
   const username = localStorage.getItem('username');
 
+  const getPdf = () => {
+    // You'll need to make your image into a Data URL
+    // Use http://dataurl.net/#dataurlmaker
+    var doc = new jsPDF();
+
+    doc.setFillColor(188, 197, 242);
+    doc.rect(0, 0, 210, 40, "F");
+
+    doc.setDrawColor(145, 145, 145);
+    doc.line(0, 40, 210, 40);
+
+    doc.setFontSize(30);
+    doc.setFontType("bold");
+    doc.setTextColor(105, 126, 244);
+    doc.text(15, 25, "Zwallet", "left");
+
+    doc.setFontSize(40);
+    doc.text(105, 25, "INVOICE", "center");
+
+    doc.setFontSize(30);
+    doc.setTextColor(0, 0, 0);
+    doc.text(195, 25, "id", "right");
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(192, 192, 192);
+    doc.text(195, 55, "Date", "right");
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(0, 0, 0);
+    doc.text(195, 65, `${transferState.date}`, "right");
+
+    // doc.setFontSize(20)
+    // doc.setFontType('normal')
+    // doc.setTextColor(0, 0, 0)
+    // doc.text(195, 75, 'time','right')
+
+    doc.setDrawColor(145, 145, 145);
+    doc.setFillColor(255, 255, 255);
+    doc.roundedRect(5, 85, 200, 150, 3, 3, "FD");
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(192, 192, 192);
+    doc.text(105, 100, "Transfer Success", "center");
+
+    doc.setFontSize(50);
+    doc.setFontType("bold");
+    doc.setTextColor(106, 127, 244);
+    doc.text(105, 120, `Rp. ${transferState.amount}`, "center");
+
+    doc.setDrawColor(145, 145, 145);
+    doc.line(5, 135, 205, 135);
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(192, 192, 192);
+    doc.text(15, 150, "Transfer to", "left");
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(0, 0, 0);
+    doc.text(195, 150, "username", "right");
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(192, 192, 192);
+    doc.text(15, 160, "Phone number", "left");
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(0, 0, 0);
+    doc.text(195, 160, "phone", "right");
+
+    doc.setDrawColor(145, 145, 145);
+    doc.line(5, 175, 205, 175);
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(192, 192, 192);
+    doc.text(15, 190, "Notes", "left");
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(0, 0, 0);
+    doc.text(15, 200, `${transferState.notes}`, "left");
+
+    doc.setFillColor(188, 197, 242);
+    doc.rect(0, 257, 210, 40, "F");
+
+    doc.setDrawColor(145, 145, 145);
+    doc.line(0, 257, 210, 257);
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(255, 255, 255);
+    doc.text(105, 275, "Powered By", "center");
+
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.setTextColor(255, 255, 255);
+    doc.text(105, 285, "© Zwallet Finance Solution", "center");
+
+    doc.save("a4.pdf");
+  };
   // const [statusTransfer, setStatusTransfer] = useState(true);
   // // console.log(setStatusTransfer);
   return (
@@ -79,7 +186,7 @@ const StatusTransferPage = () => {
             <img src={ShareIcon} alt="" />
           </button>
 
-          <button className="download-button">
+          <button className="download-button" onClick={()=>getPdf()}>
             <img className="download-icon" src={DownloadIcon} alt="" />
             Download PDF
           </button>
