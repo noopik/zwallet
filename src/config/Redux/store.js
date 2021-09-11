9ee2f3bdf1f7e542681e95f0b1ec796d5940bc08
dwinovic <nd.cahya19@gmail.com>
2021-09-11 15:30:36 +0700

    fixing redux store

--- src/config/Redux/store.js
+++ src/config/Redux/store.js
@@ -1,9 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
-import logger from 'redux-logger';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
   key: 'root',
@@ -11,19 +11,14 @@ const persistConfig = {
 };
 const persistedReducer = persistReducer(persistConfig, reducer);
 
-let middleware = [thunk];
+const middlewares = [thunk];
 
-if (process.env.NODE_ENV !== 'production') {
-  let b = require(logger);
-  middleware = [...middleware, b];
-} else {
-  middleware = [...middleware];
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+  middlewares.push(logger);
 }
 
-const createStoreWithMiddleware = applyMiddleware(...middleware);
-
-const store = createStore(persistedReducer, createStoreWithMiddleware);
-
+const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 const persistor = persistStore(store);
 
 export { store, persistor };
