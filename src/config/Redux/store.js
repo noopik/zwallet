60e4fcb9df63c75ed039d1e5c8e19870ca7871a9
dwinovic <nd.cahya19@gmail.com>
2021-09-11 15:38:08 +0700

    remove unused vars

--- src/config/Redux/store.js
+++ src/config/Redux/store.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
